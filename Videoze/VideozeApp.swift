184f81df9cc2f72cea5cf04a86821df8a244a48c
Fred Rodolfo <frodolfo@cubic.com>
2022-10-07 23:57:49 -0700

    Initial Commit

---
+++ Videoze/VideozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideozeApp.swift
+//  Videoze
+//
+//  Created by Rodolfo, Fred (US) on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct VideozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
